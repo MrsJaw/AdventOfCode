27ba24b63c6e92eb52b3f0f80a0c6787ac9b7196
Julia <MrsJaw@users.noreply.github.com>
2015-12-03 21:40:57 -0600

    Day 2
    I just found the part b puzzles...so I'll do those...later....

---
+++ Day2/Day2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FilePath = string.Empty;
+            string GiftDimensionsFilePath = string.Empty;
+            int Result = 0;
+
+            //Get Input
+            Console.Write("Merry Christmas Elves! What's the file path for your gift dimensions?  ");
+            GiftDimensionsFilePath = Console.ReadLine();
+
+            //Calculate Gift Wrap
+            if (!string.IsNullOrWhiteSpace(GiftDimensionsFilePath))
+            {
+                Result = CalculateWrappingPaperFromDimensions(GiftDimensionsFilePath);
+                if (Result > 0)
+                {
+                    Console.WriteLine("You need " + Result + " sq.ft. of wrapping paper.");
+                }
+                else
+                {
+                    Console.WriteLine("Son of a nutcracker! Something's not right with those measurements.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You cotten-headed ninnymuggins. That's not a file.");
+            }
+
+            Console.Read();
+        }
+
+        private static int CalculateWrappingPaperFromDimensions(string path)
+        {
+            int Result = 0;
+
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(path))
+                {
+                    using (StreamReader reader = new StreamReader(path))
+                    {
+                        while(!reader.EndOfStream)
+                        {
+                            string[] GiftDimensions = reader.ReadLine().Split('x');
+                            if(GiftDimensions.Length == 3)
+                            {
+                                int Lenth = Convert.ToInt32(GiftDimensions[0]);
+                                int Width = Convert.ToInt32(GiftDimensions[1]);
+                                int Height = Convert.ToInt32(GiftDimensions[2]);
+                                int Side1 = Lenth * Width;
+                                int Side2 = Width * Height;
+                                int Side3 = Lenth * Height;
+                                Result += (2 * Side1) + (2 * Side2) + (2 * Side3) + Math.Min(Side1, Math.Min(Side2, Side3));
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                Result = -1; // NEGATIVE SQUAREFOOTAGE OF WRAPPING PAPER. JUST GO WITH IT.
+            }
+
+            return Result;
+        }
+    }
+}
+
