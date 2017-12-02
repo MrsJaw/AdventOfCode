a553b376c52ea265e63e9e22cbdc37feaac39ce2
Julia <bryson.julia@gmail.com>
2017-12-02 17:44:24 +0000

    day2 part 2

--- 2017/day2.cc
+++ 2017/day2.cc
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+
 using namespace std;
 
 int main() {
     ifstream in("Input.txt");
 
+    vector<int> column;
+    vector<vector<int>> row;
     int checksum = 0;
     int rowMin = -1;
     int rowMax = 0;
     int i;
     
     while( in >> i ){
+        column.push_back(i);
         if(rowMin < 0 || i < rowMin)
             rowMin = i;
         if(i > rowMax)
@@ -20,9 +25,27 @@ int main() {
             checksum += (rowMax - rowMin);
             rowMax = 0;
             rowMin = -1;
+            row.push_back(column);
+            column.clear();
         }
     }
 
-    cout << checksum << endl;
+    cout << "Part 1: " << checksum << endl;
+    
+    int evenlyDivisibleSum = 0;
+    for(i = 0; i < row.size(); i++){
+        int columnCount = row[i].size();
+        for(int j = 0; j < columnCount; j++){
+            int operand1 = row[i][j];
+            for(int k = 0; k < columnCount; k++){
+                int operand2 = row[i][k];
+                if(j!=k && operand2!=0 && operand1%operand2==0)
+                    evenlyDivisibleSum += (operand1/operand2);
+            }
+        }
+    }
+    
+    cout << "Part 2: " << evenlyDivisibleSum << endl;
+    
     return 0;
 }   
