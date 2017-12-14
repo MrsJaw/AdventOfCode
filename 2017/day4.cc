62efe1452dd77130a7914611a59a4254cb9a6252
Julia <bryson.julia@gmail.com>
2017-12-14 16:22:28 +0000

    Day 4 Part 2

--- 2017/day4.cc
+++ 2017/day4.cc
@@ -1,47 +1,63 @@
 #include <iostream>
-#include <cmath>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <list>
+
 using namespace std;
 
-int part1(int input){
-    int result = 0;
-    
-    if(input > 1){
-        int sideLength = 1;
-        int level = 1;
-        int maxNumberOnLevel = 1;
-        while(maxNumberOnLevel < input){
-            level++;
-            sideLength += 2;
-            maxNumberOnLevel = pow(sideLength, 2);
-        }
-        
-        int stepsToNextSide = sideLength-1;
-        int closestCardinal = maxNumberOnLevel - (stepsToNextSide/2);
-        int sideMin = maxNumberOnLevel - stepsToNextSide;
-        int sideMax = maxNumberOnLevel;
-        bool found = false;
-        
-        do{
-          if(input > sideMin && input <= sideMax){
-              found = true;
-          }  
-          else{
-              closestCardinal-=stepsToNextSide;
-              sideMin -= stepsToNextSide;
-              sideMax -= stepsToNextSide;
-          }
-        }while(!found);
-        
-        result = abs(closestCardinal - input) + (level-1);
-    }
-    
-    return result;
-}
-
 int main() {
-    int input;
-    cout << "Puzzle Input: " << endl;
-    cin >> input;
-    cout << "Part 1: " << part1(input) << endl;
+    ifstream in("Input.txt");
+    int validPhrases = 0;
+    int extraValidPhrases = 0;
+    
+    string line;
+    vector<string> words;
+    string word;
+    bool valid = true;
+    bool extraValid = true;
+
+    while(getline(in, line)){
+        istringstream ssin(line);
+        while(valid && ssin >> word){
+            valid = !any_of(words.begin(), words.end(), bind2nd(equal_to<string>(), word));
+            if(valid){
+                for(int i = 0; i < words.size() && extraValid; i++){
+                    if(words[i].length() == word.length()){
+                        list<char> wordToCheck(words[i].begin(), words[i].end());
+                        int j = 0;
+                        bool charMatchFound = true;
+                        do{
+                            list<char>::iterator iter = find (wordToCheck.begin(), wordToCheck.end(), word[j++]);
+                            if(wordToCheck.end() == iter){
+                                charMatchFound = false;
+                            }else{
+                                wordToCheck.erase(iter);   
+                            }
+                        }while(j < word.length() && wordToCheck.size() > 0 && charMatchFound);
+                        if(charMatchFound && wordToCheck.size() == 0){
+                            extraValid = false;
+                        }
+                    }
+                }
+                words.push_back(word);
+            }
+        }
+        if(valid){
+            validPhrases++;
+            if(extraValid){
+                extraValidPhrases++;
+            }
+        }
+        words.clear();
+        valid = true;
+        extraValid = true;
+    }
+
+    cout << "Part 1: " << validPhrases << endl;
+    cout << "Part 2: " << extraValidPhrases << endl;
+    
     return 0;
-}
+}   
